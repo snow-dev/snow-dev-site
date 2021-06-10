c60b7f142e4301923fd016cbdd121352a84059a2
Carlos Ávila <carlos.avila@yaydoo.com>
2021-06-10 17:10:55 -0500

    Finishes Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+	return (
+		<Layout pageTitle="About Me">
+			<p>Hi there! I', Snow dev, creator of this Gatsby site</p>
+		</Layout>
+	);
+};
+
+export default AboutPage;
